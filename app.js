3fd15fb2329e76c8232756737f7a35201b7ec83b
Shai Besquar <kekashi69p@protonmail.com>
2020-08-24 14:36:41 +0300

    a

---
+++ app.js
@@ -0,0 +1,34 @@
+//var createError = require('http-errors');
+//var express = require('express');
+//var path = require('path');
+//var cookieParser = require('cookie-parser');
+//var logger = require('morgan');
+
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
+
+//var app = express();
+
+
+//app.use(logger('dev'));
+//app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//app.use(cookieParser());
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
