5af4575991effe6403e8c09cf7cf5dee0f5c3d5d
Shai Besquar <kekashi69p@protonmail.com>
2020-08-24 15:14:29 +0300

    x

--- app.js
+++ app.js
@@ -23,10 +23,10 @@
 
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('<h1>Hello World!<h1>')
 })
 
 app.listen(port, () => {
