eada1db4af9c2f9b6c60c1bfd259dd18cdb61d79
Shai Besquar <kekashi69p@protonmail.com>
2020-08-25 10:44:22 +0300

    d

--- routes/events.js
+++ routes/events.js
@@ -1,13 +1,37 @@
 var express = require('express')
 var eventRouter = express.Router()
 
+
 eventRouter.get('/',(req,res,next)=>{
-   //console.log(req.baseUrl)
+   var reqParams = {
+     limit: req.query.limit,
+     offset: req.query.offset,
+     evst: req.query.evst,
+     tkst: req.query.tkst,
+     ven: req.query.ven,
+     ha: req.query.ha
+   } 
+   console.log(reqParams)
 });
 
 eventRouter.post('/',(req,res,next)=>{
-  console.log(req.body)
+  
 });
 
+eventRouter.put('/',(req,res,next)=>{
+  
+});
+
+eventRouter.delete('/',()=>{
+
+})
+
+eventRouter.get('/:eventId(\\d+)',(req,res,next)=>{
+  
+})
+
+eventRouter.delete('/:eventId(\\d+)',(req,res,next)=>{
+  
+})
 
 module.exports = eventRouter
